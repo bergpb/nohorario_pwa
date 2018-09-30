c4bc70d3b4efcf388d2e8e350f04895f3ffc75bb
bergpb <lindembergdepaulo@gmail.com>
2018-09-29 23:52:31 -0300

    correcoes e implementacao linha-preferida

---
+++ src/views/LinhaPreferida.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-layout
+    class="horarios"
+    align-center
+    justify-center
+    fill-height
+    row
+    wrap>
+    <v-container>
+      <div class="title">Selecione uma linha de sua preferência:</div>
+      <br>
+      <v-flex
+        xs12
+        sm12
+        md12
+        lg12
+        xl12>
+        <v-btn
+          v-for="linha in linhas"
+          outline
+          large
+          color="teal"
+          @click="saveLine(linha)">
+          <strong>{{ linha.linha }}</strong>
+        </v-btn>
+      </v-flex>
+      <br>
+      <p>Obs: Os horários da linha selecionada serão mostrados na opção do menu Horários Próximos.</p>
+    </v-container>
+  </v-layout>
+</template>
+
+<script>
+import {retornaLinhasDisponiveis, salvaLocalStorage, retornaLocalStorage } from '../utils/utils.js'
+
+export default {
+  name: 'LinhaPreferida',
+  data () {
+    return {
+      linhas: null,
+      key: "preferedLine"
+    }
+  },
+  mounted () {
+    this.linhas = retornaLinhasDisponiveis();
+  },
+  methods: {
+    saveLine: function(line){
+      salvaLocalStorage(this.key, JSON.stringify(line))
+      let item = retornaLocalStorage(this.key)
+      this.$router.push({ name : 'horariosproximos', params: { item }})
+    },
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
