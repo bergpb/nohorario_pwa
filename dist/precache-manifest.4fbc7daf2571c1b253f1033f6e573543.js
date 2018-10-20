1d7e0426c0e195fe3ed4435e978a7f762ac79f0f
bergpb <lindembergdepaulo@gmail.com>
2018-10-20 15:50:23 -0300

    dist adicionado ao projeto

---
+++ dist/precache-manifest.4fbc7daf2571c1b253f1033f6e573543.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "369b282d1de5f621fc90",
+    "url": "/css/chunk-cfeb.bdc4c00a.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "369b282d1de5f621fc90",
+    "url": "/js/chunk-cfeb.02d64740.js"
+  },
+  {
+    "revision": "91d5ee64e20276c47c8c",
+    "url": "/css/chunk-32d0.5a368e2b.css"
+  },
+  {
+    "revision": "902bf0af3474f6aeea74",
+    "url": "/css/chunk-49bb.53412cec.css"
+  },
+  {
+    "revision": "902bf0af3474f6aeea74",
+    "url": "/js/chunk-49bb.9150f57d.js"
+  },
+  {
+    "revision": "c0b3322d1fb9935c0939",
+    "url": "/css/chunk-6a85.c0af6408.css"
+  },
+  {
+    "revision": "c0b3322d1fb9935c0939",
+    "url": "/js/chunk-6a85.854daf9e.js"
+  },
+  {
+    "revision": "535028134dc0c7b5fd79",
+    "url": "/css/chunk-c22d.214f1e14.css"
+  },
+  {
+    "revision": "535028134dc0c7b5fd79",
+    "url": "/js/chunk-c22d.0ce45d42.js"
+  },
+  {
+    "revision": "21fc3b4adbcf998fdde8",
+    "url": "/js/app.2edd1cb6.js"
+  },
+  {
+    "revision": "91d5ee64e20276c47c8c",
+    "url": "/js/chunk-32d0.25cb3b4e.js"
+  },
+  {
+    "revision": "a343f6848bd799252fc5",
+    "url": "/css/chunk-vendors.a4b0507a.css"
+  },
+  {
+    "revision": "a343f6848bd799252fc5",
+    "url": "/js/chunk-vendors.e6bc240e.js"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "/fonts/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "/fonts/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "/fonts/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/fonts/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "8f66d11fd682e37454babb55d05f329c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "297c567ccb78cd7870292d7ce8117b37",
+    "url": "/img/hqdefault.jpg"
+  },
+  {
+    "revision": "21fc3b4adbcf998fdde8",
+    "url": "/css/app.cbe36d97.css"
+  }
+];
